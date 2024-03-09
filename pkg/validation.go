95eeaac672efb7e14b2ce87cdb49cafc41395f97
nikhilsbhat <nikhilsbhat93@gmail.com>
2024-03-09 13:47:07 +0530

    Fix the prerequisite validation
    Previously it was checking for the presense of golang instead of kubectl

--- pkg/validation.go
+++ pkg/validation.go
@@ -8,7 +8,7 @@ import (
 func (drift *Drift) ValidatePrerequisite() bool {
 	success := true
 
-	if goPath := exec.Command("go"); goPath.Err != nil {
+	if goPath := exec.Command("kubectl"); goPath.Err != nil {
 		if !errors.Is(goPath.Err, exec.ErrDot) {
 			drift.log.Infof("%v", goPath.Err.Error())
 			drift.log.Info("helm-drift requires 'kubectl' to identify drifts")
