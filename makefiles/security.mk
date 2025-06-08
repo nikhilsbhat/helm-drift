6a3cb5da956c7db41499f80e6fbab4b2e5234ca9
nikhilsbhat <nikhilsbhat93@gmail.com>
2025-06-08 13:04:00 +0530

    Update the trivy configurations

--- makefiles/security.mk
+++ makefiles/security.mk
@@ -1,5 +1,10 @@
 scan/code: ## scans code for vulnerabilities
-	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy fs . --scanners vuln
+	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy fs /helm-drift
 
+<<<<<<< HEAD
 scan/binary: mock/publish ## scans binary for vulnerabilities
 	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy config --input dist/helm-drift_darwin_amd64_v1/helm-drift
+=======
+scan/binary: ## scans binary for vulnerabilities
+	@docker-compose --project-name trivy -f docker-compose.trivy.yml run --rm trivy fs /helm-drift/dist/helm-drift_darwin_amd64_v1/helm-drift --scanners vuln
+>>>>>>> 860c293 (Update the trivy configurations)
